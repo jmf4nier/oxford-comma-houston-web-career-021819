1726f8d43d4e593a63ee854d53df3e6df7aa3cb8
Jason Fournier <jmf4nier@hotmail.com>
2019-02-01 18:15:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,6 @@
 array = ["fiddleheads","okra","kohlrabi", "something else"]
 def oxford_comma(array)
+ 
  oxford = array.pop
  last_plus_and = "and " + oxford      #adds "and" to the .pop return
  array.push(last_plus_and)            #pushes "and + .pop" to the end of the array
