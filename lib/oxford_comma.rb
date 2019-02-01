0b97725f9065030052ecdd81e2718568da635872
Jason Fournier <jmf4nier@hotmail.com>
2019-02-01 18:12:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
-array = ["fiddleheads","okra","kohlrabi"]
+array = ["fiddleheads","okra","kohlrabi", "something else"]
 def oxford_comma(array)
  oxford = array.pop
  last_plus_and = "and " + oxford
