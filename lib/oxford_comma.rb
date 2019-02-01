8be8a6db6b759d5d59621dfa7c03ef3ab0863d72
Jason Fournier <jmf4nier@hotmail.com>
2019-02-01 18:22:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,13 @@
 array = ["fiddleheads","okra"]
 def oxford_comma(array)
  if array.size > 2
- oxford = array.pop
- last_plus_and = "and " + oxford      #adds "and" to the .pop return
- array.push(last_plus_and)            #pushes "and + .pop" to the end of the array
- string = array.join(", ")
- p string
+  oxford = array.pop
+  last_plus_and = "and " + oxford      #adds "and" to the .pop return
+  array.push(last_plus_and)            #pushes "and + .pop" to the end of the array
+  string = array.join(", ")
+  p string
+ else
+   
 end
 oxford_comma(array)
  
